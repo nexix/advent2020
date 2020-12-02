5a67d8bf6936a9c2f4fcb14a8a7eace509b9307a
nexix <rob.quix@gmail.com>
2020-12-02 15:54:16 -0700

    switching to xor

--- p2-2.py
+++ p2-2.py
@@ -8,15 +8,14 @@ for line in f:
 	print(cmin,cmax,letter,password, end='')
 	cmin_found = False
 	cmax_found = False
-	if letter == password[int(cmin)-1]:
-		#print("CMIN ::",int(cmin)-1,password[int(cmin)-1])
-		cmin_found = True
-	if letter == password[int(cmax)-1]:
-		#print("CMAX ::",int(cmax)-1,password[int(cmax)-1])
-		cmax_found = True
-	if (cmin_found == True and cmax_found == True):
-		pass
-	elif (cmin_found == True or cmax_found == True):
+	if bool(letter == password[int(cmin)-1]) ^ bool(letter == password[int(cmax)-1]):
 		print("VALID\n")
 		valid = valid + 1
 print("VALID ::", valid)
+
+#xor truth table:
+#A B R
+#F F F
+#F T T
+#T F T
+#T T F
